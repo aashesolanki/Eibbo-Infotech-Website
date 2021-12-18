9a47db25219575f1cb0ac6ca065c843103fc1988
aashesolanki <48376708+aashesolanki@users.noreply.github.com>
2021-12-18 02:53:38 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+   
+    <link rel="stylesheet" href="style.css"/>
+
+    <title>Eibbo Infotech</title>
+
+</head>
+
+<body>
+
+
+<!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3">
+        <div class="container">
+
+        <a href="#" class="navbar-brand">Eibbo Infotech</a>
+            
+        <button 
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navmenu"
+        >
+
+        <span class="navbar-toggler-icon"></span></span>
+            
+        </button>
+
+        <div class="collapse navbar-collapse" id="navmenu">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a href="#1" class="nav-link">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#2" class="nav-link">Services</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#3" class="nav-link">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+
+
+<!-- section 1  start -->
+
+<section class="bg-dark text-light p-5 p-lg-0 pt-lg-5 text-center text-sm-start">
+    <div class="container">
+        <div class="d-sm-flex align-items-center justify-content-between">
+            <div>
+                <h1>HELLO, I'M <span class="text-warning">EIBBO</span></h1>
+                <p class="lead my-4">
+                We help the brands to become what they want and designing success through creative web design.
+                Pay less and get the best designed applications.
+                </p>
+
+                <button class="btn btn-primary btn-lg">Contact Us</button>
+
+
+<!-- Section 1 Image -->
+
+        </div>        
+            <img class="img-fluid w-50 d-none d-sm-block" src= "showcase.svg" alt="background image">
+        </div>
+
+    </div>
+</section>
+
+
+<!-- section 2 start -->
+
+<section class="bg-primary text-light p-5">
+    <div class="container">
+        <div class="d-md-flex justify-content-between align-items-center">
+            <h3 class="mb-3 mb-md-0">
+                Sign Up For Our Newsletter
+            </h3>
+            <div class="input-group news-input">
+                <input 
+                    type="text" 
+                    class="form-control"
+                    placeholder="Enter Email"
+                />
+                <button
+                    class="btn btn-dark btn-lg"
+                    type="button"
+                >Submit</button>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- section 3 Boxes -->
+
+    <section class="p-5">
+        <div class="container">
+            <div class="row text-center g-4">
+
+                <!-- CARD 1 -->
+
+                <div class="col-md">
+                    <div class="card bg-dark text-light">
+                        <div class="card-body text-center">
+                            <div class="h1 mb-3">
+                                <i class="bi bi-laptop"></i>
+                            </div>
+                            <h3 class="card-title mb-3">
+                                Web Development
+                            </h3>
+                            <p class="card-text">
+                                Lorem Ipsum sdn jsdsnadjn d asn dasjndjas djasn dasj ndj
+                            </p>
+                            <a href="#" class="btn btn-primary">read more..</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- CARD 2 -->
+
+                
+                <div class="col-md">
+                    <div class="card bg-dark text-light">
+                        <div class="card-body text-center">
+                            <div class="h1 mb-3">
+                                <i class="bi bi-phone"></i>
+                            </div>
+                            <h3 class="card-title mb-3">
+                                Mobile Applications
+                            </h3>
+                            <p class="card-text">
+                            Lorem Ipsum sdn jsdsnadjn d asn dasjndjas djasn dasj ndj
+                            </p>
+                            <a href="#" class="btn btn-primary">read more..</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- CARD 3 -->
+               
+                <div class="col-md">
+                    <div class="card bg-dark text-light">
+                        <div class="card-body text-center">
+                            <div class="h1 mb-3">
+                                <i class="bi bi-globe"></i>
+                            </div>
+                            <h3 class="card-title mb-3">
+                                Digital Marketing
+                            </h3>
+                            <p class="card-text">
+                            Lorem Ipsum sdn jsdsnadjn d asn dasjndjas djasn dasj ndj
+                            </p>
+                            <a href="#" class="btn btn-primary">read more..</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+<!-- Section 4 -->
+<section id="learn" class="p-5">
+    <div class="container">
+        <div class="row align-items-center justify-content-between">
+            <div class="col-md">
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
